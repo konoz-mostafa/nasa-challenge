bfb9b78cba7e59812249e8989f5e195f3c3bdaee
konoz <konozmostafasedky@gmail.com>
2025-09-27 05:00:27 +0300

    colour

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import WorldMap from "./components/WorldMap";
 import WelcomePage from "./pages/home/WelcomePage";
 import RegistrationLogin from "./pages/auth/RegistrationLogin";
-import HUbbleExperience from "./pages/Hubble/Hubble";
+// import HUbbleExperience from "./pages/Hubble/Hubble";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,12 +13,9 @@ function App() {
       <Routes>
         <Route path="/Explora" element={<WelcomePage />} />
         <Route path="/Explora/login" element={<RegistrationLogin />} />
-<<<<<<< HEAD
-        <Route path="/" element={<WorldMap />} />
-        // <Route path="/HUbbleExperience" element={<HUbbleExperience />} />
-=======
         <Route path="/Explora/home" element={<WorldMap />} />
->>>>>>> e5d0ef8 (Update ProfileModal, Sidebar, WorldMap, and styles)
+        
+
       </Routes>
     </Router>
   );
